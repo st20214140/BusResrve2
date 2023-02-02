a943e83ea33f5cc945b34d145edc2fa557b2f976
KCSNET\st20214141 <20214141@kcska.onmicrosoft.com>
2023-02-02 12:21:03 +0900

    UserBean

--- BusReserve/src/main/java/model/UserBean.java
+++ BusReserve/src/main/java/model/UserBean.java
@@ -1,7 +1,41 @@
 package model;
 
 public class UserBean {
-	private String user_id;
-	private String user_name;
-	private String tel_number;
+	private String userId;
+	private String userName;
+	private String callNumber;
+	
+	public UserBean(String userId) {
+		this.setUserId(userId);
+	}
+	
+	public UserBean(String userName, String callNumber) {
+		this.setUserName(userName);
+		this.setCallNumber(callNumber);
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getCallNumber() {
+		return callNumber;
+	}
+
+	public void setCallNumber(String callNumber) {
+		this.callNumber = callNumber;
+	}
+	
 }
