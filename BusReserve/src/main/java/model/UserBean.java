7cb24a7debff6324ac58491f5acf4b63eee5aac6
st20214140 <20214140@kcska.onmicrosoft.com>
2023-02-07 12:23:02 +0900

    UserBean0207

--- BusReserve/src/main/java/model/UserBean.java
+++ BusReserve/src/main/java/model/UserBean.java
@@ -4,4 +4,22 @@ public class UserBean {
 	private String user_id;
 	private String user_name;
 	private String tel_number;
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getTel_number() {
+		return tel_number;
+	}
+	public void setTel_number(String tel_number) {
+		this.tel_number = tel_number;
+	}
 }
