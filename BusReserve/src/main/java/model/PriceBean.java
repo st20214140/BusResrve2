572db1eb95e149ac04e03514abb5d31ec075e986
st20214140 <20214140@kcska.onmicrosoft.com>
2023-02-07 11:17:54 +0900

    PriceBean作成

---
+++ BusReserve/src/main/java/model/PriceBean.java
@@ -0,0 +1,18 @@
+package model;
+
+public class PriceBean {
+	private String price_id;
+	private int price;
+	public String getPrice_id() {
+		return price_id;
+	}
+	public void setPrice_id(String price_id) {
+		this.price_id = price_id;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
