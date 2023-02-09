fcfe2d4d95e4816acda44491b802825e4b71507c
KCSNET\st20214141 <20214141@kcska.onmicrosoft.com>
2023-02-09 09:41:06 +0900

    UserDAO(test)

--- BusReserve/src/main/java/dao/UserDAO.java
+++ BusReserve/src/main/java/dao/UserDAO.java
@@ -8,8 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.EmployeeBean;
-import model.user;
+import model.UserBean;
 
 //import java.sql.*;
 
@@ -18,50 +17,94 @@ public class UserDAO {
 	private final String JDBC_URL = "jdbc:mysql://fukushima-pc/BusReserve";
 	private final String DB_USER = "teamE";
 	private final String DB_PASS = "eggfruitMySQL";
+	
+	/**
+	 *  findAll()メソッド<br>
+	 *  Usersテーブルから
+	 * @return
+	 */
+	public List<UserBean> findAll() {
+		List<UserBean> userList = new ArrayList<UserBean>();
 		
-	public String getUserId(user user) {
+		// データベースへ接続
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+			// SELECT文を準備
+			String sql = "SELECT user_id, user_name, call_number FROM users";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			// SELECTを実行し、結果票を取得
+			ResultSet rs = pStmt.executeQuery();
+			
+			// 結果表に格納されたレコードの内容を
+			// Employeeインスタンスに設定し、ArrayListインスタンスに追加
+			while (rs.next()) {
+				String userId   = rs.getString("user_id");
+				String userName = rs.getString("user_name");
+				String callNumber = rs.getString("call_number");
+				UserBean UserBean = new UserBean(userId, userName, callNumber);
+				userList.add(UserBean);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return userList;
+	}
+	
+	/**
+	 findUser()メソッド<br>
+	 * userName, callNumberからUserIDを検索(SELECT)します。<br>
+	 * @param UserBean
+	 * @return boolean (成功時:True / 失敗時:False)
+	 */
+	public List<UserBean> findUserID(UserBean user) {
+		List<UserBean> userList = new ArrayList<UserBean>();
+		
 		// DB接続
 		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 			// SELECT文を準備
 			String sql = 
-					"SELECT user_id " + 
-					"FROM   users" + 
-					"WHERE name = ? AND call_number = ?";
+					"SELECT user_id FROM users WHERE user_name = '?' AND call_number = '?'";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1, user.getName());
+			pStmt.setString(1, user.getUserName());
 			pStmt.setString(2, user.getCallNumber());
 			
 			// SELECTを実行し、結果票を取得
 			ResultSet rs = pStmt.executeQuery();
 			
-			return rs.getString("user_id");
+			while (rs.next()) {
+				String userId   = rs.getString("user_id");
+				UserBean userBean = new UserBean(userId);
+				userList.add(userBean);
+			}
+			
+			return userList;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	public boolean insert(user user) {
+	public boolean insert(UserBean user) {
 		// データベースへ接続
+		boolean result;
 		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 			
 			// INSERT文を接続
-			String sql = "INSERT INTO users(user_id, users, call_number) VALUES (?,?,?)";
+			String sql = 
+					"INSERT INTO users(user_id, user_name, call_number)"
+					+ "SELECT COALESCE(MAX(user_id)+1,1), ?, ?"
+					+ "FROM users";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			
 			// INSERT文の中の「?」に使用する値をセットし、SQLを組み立て
-			pStmt.setString(1, );
-			pStmt.setString(2, user.getName());
-			pStmt.setInt(3, user.getCallNumber());
+			pStmt.setString(1, user.getUserName());
+			pStmt.setString(2, user.getCallNumber());
 			
 			// INSER文を実行し、実行結果をresultに格納
-			int result = pStmt.executeUpdate();
+			result = true;
 			
-			// 成功すると1が戻るなで、1ではない時失敗
-			if (result != 1) {
-				return false;
-			}
 		} catch (SQLException e) {
 			System.out.println("!! レコードは格納(INSERT)されませんでした。");
 			e.printStackTrace();
@@ -69,6 +112,6 @@ public class UserDAO {
 		}
 		
 		System.out.println("!! レコードは正常に追加(INSERT)されました。");
-		return true;
+		return result;
 	}
 }
